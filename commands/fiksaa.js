71f96ce7ebe9b9ec602415a524a4e306940ed4a8
unknown <thesilentcoder@outlook.com>
2023-04-18 14:32:29 +0300

    Remove interaction

--- commands/fiksaa.js
+++ commands/fiksaa.js
@@ -4,9 +4,9 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('fiksaa')
     .setDescription('Fiksaa leipäkone.'),
-  async execute(interaction) {
-    if (interaction.client.borked) {
-      interaction.client.borked = false
+  async execute(interaction, client) {
+    if (client.borked) {
+      client.borked = false
       return 'Korjasit leipäkoneen! Mahtavaa!'
     }
     else {
